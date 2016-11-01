bbe0b20d22d40d9ab9ed113fa5e5c77d2df5026c
Daniela Simões <danielasimoes@ua.pt>
2016-11-01 17:24:53 +0000

    Primeira versão: a cobra ainda não foge da outra nem dela própria

--- src/map.py
+++ src/map.py
@@ -1,58 +1,100 @@
+import numpy as np
+import itertools
+import scipy.spatial.distance as d
+from tree_search import *
+
 class Map:
     def __init__(self, mapsize, maze):
         self.connections = []
-        self.coordinates = {}
+        self.coordinates = None
         self.mapsize = mapsize
+        self.Xsize = mapsize[0]
+        self.Ysize = mapsize[1]
         self.maze = maze
         self.dist_to_walk = 1
         self.instance()
 
-    def update(self, mapsize, maze):
-        if mapsize != self.mapsize:
-            print("mudou")
+    def update(self, mapsize, maze, body):
+        #start guide: the old position of the player will be added and the new position will be deleted
+        #adversario maze.playerpos[0]
+        #our snake : body
+        print(self.connections)
+        #print(any([body[0] in self.connections[1]]))
+        if any([body in self.connections[1]]):
+            self.connections.remove(self, body)
+            print("PASEI")
 
-        print("ok")
 
     def pathlen(self, a, b):
         return int(((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2) ** 0.5)
 
     def instance(self):
-        for x in range(0,self.mapsize[0]):
-            for y in range(0, self.mapsize[1]):
-                init_x = 0
-                init_y = 0
-                if (x,y) not in self.coordinates:
-                    self.coordinates[(x,y)] = dict()
-
-                for x_to in range(init_x,self.mapsize[0]):
-                    for y_to in range(init_y, self.mapsize[1]):
-                        dist = self.pathlen((x,y), (x_to,y_to))
-                        self.coordinates[(x,y)][(x_to,y_to)] = dist
-
-                        if( y_to < self.mapsize[0] and x_to < self.mapsize[1]):
-                            self.coordinates[(x,y)][(y_to,x_to)] = dist
-
-                        if (y < self.mapsize[1]):
-                            init_y += 1
-                    if (x < self.mapsize[0]):
-                        init_x += 1
-
-
+        Xpts = np.arange(self.Xsize)
+        Ypts = np.arange(self.Ysize)
+        coords = np.array(list(itertools.product(Xpts, Ypts)))
+        self.coordinates = d.cdist(coords, coords)
 
         # adicionar as conexoes
-
-        for i in range(0, self.mapsize[0]*self.mapsize[1]):
+        for x,y in coords:
             if (x,y) not in self.maze.obstacles:
                 options = [(x, y + self.dist_to_walk), (x, y - self.dist_to_walk), (x + self.dist_to_walk, y), (x - self.dist_to_walk, y)]
 
                 for i in options:
                     if i not in self.maze.obstacles:
-                        self.connections += [((x,y),i)] # as lista de conexoes e um tuplo com duas coordenadas: onde estou e onde posso ir
-        print("sai")
+                        if i[0]<0:  # if the map does not continue to the left, snake returns from the right side
+                            self.connections += [((x, y), (i[0] + self.Xsize, i[1]))]
+                        elif i[1] < 0:  # if the map does not continue to the top, snake returns from the bottom side
+                            self.connections += [((x, y), (i[0], i[1] + self.Ysize))]
+                        elif i[0] > self.Xsize: # if the map does not continue to the right, snake returns from the left side
+                            self.connections += [((x,y), (i[0]%self.mapsize[0], i[1]))]
+                        elif i[1] > self.Ysize: # if the map does not continue to the right, snake returns from the left side
+                            self.connections += [((x, y), (i[0], i[1] % self.mapsize[1]))]
+                        else:
+                            self.connections += [((x, y), i)]
 
 
 
+class Way:
+    def __init__(self, map):
+        self.map = map
+        self.prob = GridConnections(map)
+        self.calculated = None
+
+    def search_path(self, from_point, to):
+        prob = SearchProblem(self.prob, from_point, to)
+        tree = SearchTree(prob)
+        self.calculated = tree.search()
+        return self.calculated
 
 
+class GridConnections(SearchDomain):
+    def __init__(self, map):
+        SearchDomain.__init__(self)
+        self.connections = map.connections
+        self.coordinates = map.coordinates
+        self.map = map
+        self.visited = []
 
+    def actions(self, cell):
+        self.visited += [cell]
+        actlist = []
+        for c1, c2 in self.connections:
+            if c1 == cell and c2 not in self.visited:
+                actlist += [(c1, c2)]
+            elif c2 == cell and c1 not in self.visited:
+                actlist += [(c2, c1)]
+        return actlist
 
+    def result(self, state, action):
+        c1, c2 = action
+        if c1 == state:
+            return c2
+
+    def cost(self, state, action):
+        return 0
+
+    def heuristic(self, state, goal_state):
+        return self.coordinates[self.to_index(state, self.map.mapsize[1]), self.to_index(goal_state, self.map.mapsize[1])]
+
+    def to_index(self, point, mapsize_y):
+        return point[0] * mapsize_y + point[1]
---
+++ src/student.py
@@ -0,0 +1,59 @@
+from snake import Snake
+from constants import *
+from map import Map, Way
+
+
+class Student(Snake):
+    def __init__(self, body=[(0, 0)], direction=(1, 0)):
+        # criar um mapa
+        self.map = None
+        self.maze = None
+        super().__init__(body, direction, name="DC")
+
+    def pathlen(self, a, b):
+        return int(((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2) ** 0.5)
+
+    def add(self, a, b):
+        return a[0] + b[0], a[1] + b[1]
+
+    def sub(self, a, b):
+        return a[0] - b[0], a[1] - b[1]
+
+    def update(self, points=None, mapsize=None, count=None):
+        if self.map is None:
+            self.map = Map(mapsize, self.maze)
+        else:
+            self.map.update(mapsize, self.maze, self.body)
+
+    def updateDirection(self, maze):
+        # this is the brain of the snake player
+        olddir = self.direction
+        position = self.body[0]
+
+        self.maze = maze
+
+        if self.map is None:
+            # new direction can't be up if current direction is down...and so on
+            complement = [(up, down), (down, up), (right, left), (left, right)]
+            invaliddir = [x for (x, y) in complement if y == olddir]  # o que nao pode acontecer
+            validdir = [dir for dir in directions if not (dir in invaliddir)]
+
+            # get the list of valid directions for us - isto e, se nao for uma posicao valida entao e um obstaculo ou outro jogador
+            validdir = [dir for dir in validdir if
+                        not (self.add(position, dir) in maze.obstacles or self.add(position, dir) in maze.playerpos)]
+            # if we collide then set olddir to first move of validdir (if validdir is empty then leave it to olddir)
+            olddir = olddir if olddir in validdir or len(validdir) == 0 else validdir[0]
+            # shortest path.....we assume that the direction we are currently going now gives the shortest path
+            shortest = self.pathlen(self.add(position, olddir), maze.foodpos)  # length in shortest path
+            for dir in validdir:
+                newpos = self.add(position, dir)
+                newlen = self.pathlen(newpos, maze.foodpos)  # length in shortest path
+                if newlen < shortest:
+                    olddir = dir
+                    shortest = newlen
+            self.direction = olddir
+            print(self.direction)
+        else:
+            way = Way(self.map)
+            path = way.search_path(position, maze.foodpos)
+            self.direction = self.sub(path[1], position)
