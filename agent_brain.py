5acba1a2ec3b8b9606a200c84094fd1565126da8
Daniela Simões <danielasimoes@ua.pt>
2016-11-20 13:09:24 +0000

    refactor code

--- agent_brain.py
+++ agent_brain.py
@@ -59,7 +59,7 @@ class AgentBrain:
 
     def get_direction(self, from_point, to):
         self.visited = []
-        tree = SearchTree(SearchProblem(self, from_point, to))
+        tree = SearchTree(self, from_point, to)
         return tree.search()
 
     def actions(self, cell, avoidance=True):
@@ -97,28 +97,19 @@ class AgentBrain:
         goal_state_index = self.to_index(goal_state)
         state_index = self.to_index(state)
 
-        real_distance = self.distance[state_index,
-                                            goal_state_index]
+        real_distance = self.distance[state_index, goal_state_index]
 
-        go_top_distance = self.distance[state_index,
-                                              self.to_index((state[0], self.y_limit))]
-        go_top_distance += self.distance[self.to_index((state[0], 0)),
-                                               goal_state_index]
+        go_top_distance = self.distance[state_index, self.to_index((state[0], self.y_limit))]
+        go_top_distance += self.distance[self.to_index((state[0], 0)), goal_state_index]
 
-        go_bottom_distance = self.distance[state_index,
-                                                 self.to_index((state[0], 0))]
-        go_bottom_distance += self.distance[self.to_index((state[0], self.y_limit)),
-                                                  goal_state_index]
+        go_bottom_distance = self.distance[state_index, self.to_index((state[0], 0))]
+        go_bottom_distance += self.distance[self.to_index((state[0], self.y_limit)), goal_state_index]
 
-        go_left_distance = self.distance[state_index,
-                                               self.to_index((0, state[1]))]
-        go_left_distance += self.distance[self.to_index((self.x_limit, state[1])),
-                                                goal_state_index]
+        go_left_distance = self.distance[state_index, self.to_index((0, state[1]))]
+        go_left_distance += self.distance[self.to_index((self.x_limit, state[1])), goal_state_index]
 
-        go_right_distance = self.distance[state_index,
-                                                self.to_index((self.x_limit, state[1]))]
-        go_right_distance += self.distance[self.to_index((0, state[1])),
-                                                 goal_state_index]
+        go_right_distance = self.distance[state_index, self.to_index((self.x_limit, state[1]))]
+        go_right_distance += self.distance[self.to_index((0, state[1])), goal_state_index]
 
         return min(real_distance, go_top_distance, go_bottom_distance, go_left_distance, go_right_distance)
 
@@ -132,16 +123,6 @@ class AgentBrain:
     def head_collision_avoidance(self, cell):
         result = np.where((self.head_collision_matrix == cell).all(axis=1))
         return not (len(result[0]) and len(result[1]))
-
-
-class SearchProblem:
-    def __init__(self, domain, initial, goal):
-        self.domain = domain
-        self.initial = initial
-        self.goal = goal
-
-    def goal_test(self, state):
-        return state == self.goal
 
 
 class SearchNode:
@@ -159,9 +140,11 @@ class SearchNode:
 
 
 class SearchTree:
-    def __init__(self, problem):
-        self.problem = problem
-        root = SearchNode(problem.initial, None, self.problem.domain.heuristic(self.problem.initial, self.problem.goal))
+    def __init__(self, domain, initial, goal):
+        self.domain = domain
+        self.initial = initial
+        self.goal = goal
+        root = SearchNode(initial, None, self.domain.heuristic(self.initial, self.goal))
         self.open_nodes = [root]
         self.result = []
         self.node = root
@@ -173,6 +156,9 @@ class SearchTree:
             return [node.state]
 
         return self.get_path(node.parent) + [node.state]
+
+    def goal_test(self, state):
+        return state == self.goal
 
     def search_helper(self):
         visited = []
@@ -186,7 +172,7 @@ class SearchTree:
 
             self.result = self.get_path(self.node)
 
-            if self.problem.goal_test(self.node.state):
+            if self.goal_test(self.node.state):
                 signal.alarm(0)
                 return
 
@@ -196,17 +182,17 @@ class SearchTree:
             visited += [self.node.state]
             lnewnodes = []
 
-            self.actions = self.problem.domain.actions(self.node.state)
+            self.actions = self.domain.actions(self.node.state)
 
             if len(self.actions) == 0 and len(self.open_nodes) == 0:
                 print("avoidance")
-                self.actions = self.problem.domain.actions(self.node.state, avoidance=False)
+                self.actions = self.domain.actions(self.node.state, avoidance=False)
             # elif len(actions) == 0:
             #   print("ELSE", self.problem.domain.name, self.problem.goal, self.problem.initial, self.open_nodes)
 
             for newstate in self.actions:
                 if newstate not in self.result:
-                    heuristic = self.problem.domain.heuristic(newstate, self.problem.goal)
+                    heuristic = self.domain.heuristic(newstate, self.goal)
                     lnewnodes += [SearchNode(newstate, self.node, heuristic)]
             self.add_to_open(lnewnodes)
 
@@ -217,7 +203,7 @@ class SearchTree:
 
     def search(self):
         signal.signal(signal.SIGALRM, self.signal_handler)
-        agent_time = (self.problem.domain.agent_time/1000)*(4/5)
+        agent_time = (self.domain.agent_time/1000)*(4/5)
 
         signal.setitimer(signal.ITIMER_REAL, agent_time)
 
@@ -229,22 +215,22 @@ class SearchTree:
             signal.alarm(0)
 
         # get the direction
-        direction = self.problem.domain.direction
+        direction = self.domain.direction
 
         if len(self.result) > 1:
-            direction = sub(self.result[1], self.problem.domain.body[0])
+            direction = sub(self.result[1], self.domain.body[0])
         elif len(self.result) == 1:
-            print("actions", self.actions, "open_nodes", self.open_nodes, "state", self.problem.initial, "goal",
-                  self.problem.goal, "atual", self.node)
+            print("actions", self.actions, "open_nodes", self.open_nodes, "state", self.initial, "goal",
+                  self.goal, "atual", self.node)
 
-            print(self.problem.domain.name, "size 1", "path", self.result)
+            print(self.domain.name, "size 1", "path", self.result)
 
         if direction[0] > 1 or direction[0] < -1:
-            direction = -int(self.problem.domain.x_size * 1.0 / direction[0]), direction[1]
+            direction = -int(self.domain.x_size * 1.0 / direction[0]), direction[1]
         if direction[1] > 1 or direction[1] < -1:
-            direction = direction[0], -int(self.problem.domain.y_size * 1.0 / (direction[1]))
+            direction = direction[0], -int(self.domain.y_size * 1.0 / (direction[1]))
 
-        self.problem.domain.direction = direction
+        self.domain.direction = direction
 
         return direction
 
